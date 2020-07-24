f8707afdf636554aa4ec4a246b9304caf7adf0a6
junner <junnerff@gmail.com>
2020-07-23 23:45:07 -0500

    json added in js for contain each post then show it on document

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 "strict mode"
+
 $(document).ready(function () {
 
-    const slider = $(function () {
+    //function that allow us to show a image slider (thanks to bxslider plugin)
+
+    const slider = () => {
         $('.bxslider').bxSlider({
             mode: 'fade',
             captions: true,
@@ -10,7 +13,37 @@ $(document).ready(function () {
             randomStart: true,
             auto: true
         });
-    });
+    };
 
     slider();
+
+    //JSON that stores all posts in JSON format
+    let jsonPost = [
+        { title: 'title test 1', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 1' },
+        { title: 'title test 2', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 2' },
+        { title: 'title test 3', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 3' },
+        { title: 'title test 4', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 4' }
+
+    ];
+
+    //Then each jsonPost goes into an HTML structure for it show
+    jsonPost.forEach(element => {
+        let htmlPost = `
+        <article class="post">
+            <h2>${element.title}</h2>
+            <span class="date">${element.date}</span>
+            <p>
+              ${element.content}
+            </p>
+            <a href="#" class="button-more">Read more</a>
+          </article>
+          <hr />
+        `;
+        //adding every post
+        $("#posts").append(htmlPost);
+    });
+
+
+
+
 });
