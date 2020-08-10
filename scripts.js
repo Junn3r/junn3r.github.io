9cc64e947f53a16af15fba53c32060413c5925f3
junner <junnerff@gmail.com>
2020-08-10 01:12:06 -0500

    now we have a new function named onDomain() that will notice us if the page is loading on a github domain or not

--- scripts.js
+++ scripts.js
@@ -1,41 +1,64 @@
 "strict mode"
 
 $(document).ready(function () {
+    //function that returns true if the page is loading on github domain
+    function onDomain() {
+        if (window.location.href.indexOf('https://junn3r.github.io/') != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     //function that allow us to show a image slider (thanks to bxslider plugin)
+    //First we validate if the page is loading on github pages domain
 
-    console.log(window.location.href);
-    
+    if (onDomain()) {
+        if (window.location.href == 'https://junn3r.github.io/index.html' || window.location.href == 'https://junn3r.github.io/') {
+            const slider = () => {
+                $('.bxslider').bxSlider({
+                    mode: 'fade',
+                    captions: true,
+                    slideWidth: 1200,
+                    adaptiveHeight: true,
+                    randomStart: true,
+                    auto: true
+                });
+            };
 
-    if (window.location.href.indexOf('index.html') != -1 || window.location.href.indexOf('https://junn3r.github.io/') != -1) {
-        const slider = () => {
-            $('.bxslider').bxSlider({
-                mode: 'fade',
-                captions: true,
-                slideWidth: 1200,
-                adaptiveHeight: true,
-                randomStart: true,
-                auto: true
-            });
-        };
-
-        slider();
+            slider();
+        }
+    } else {
+        if (window.location.href.indexOf('index.html') != -1) {
+            const slider = () => {
+                $('.bxslider').bxSlider({
+                    mode: 'fade',
+                    captions: true,
+                    slideWidth: 1200,
+                    adaptiveHeight: true,
+                    randomStart: true,
+                    auto: true
+                });
+            };
+            slider();
+        }
 
     }
 
-    if (window.location.href.indexOf('index.html') != -1 || window.location.href.indexOf('https://junn3r.github.io/') != -1) {
-        //JSON that stores all posts in JSON format
 
-        let jsonPost = [
-            { title: 'title test 1', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 1' },
-            { title: 'title test 2', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 2' },
-            { title: 'title test 3', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 3' },
-            { title: 'title test 4', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 4' }
+    if (onDomain()) {
+        if (window.location.href == 'https://junn3r.github.io/index.html' || window.location.href == 'https://junn3r.github.io/') {
+            //JSON that stores all posts in JSON format
+            let jsonPost = [
+                { title: 'title test 1', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 1' },
+                { title: 'title test 2', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 2' },
+                { title: 'title test 3', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 3' },
+                { title: 'title test 4', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 4' }
 
-        ];
-
-        jsonPost.forEach(element => {
-            let htmlPost = `
+            ];
+            //Then each jsonPost goes into an HTML structure for it show
+            jsonPost.forEach(element => {
+                let htmlPost = `
             <article class="post">
                 <h2>${element.title}</h2>
                 <span class="date">${element.date}</span>
@@ -46,28 +69,72 @@ $(document).ready(function () {
               </article>
               <hr />
             `;
-            //adding every post
-            $("#posts").append(htmlPost);
+                //adding every post
+                $("#posts").append(htmlPost);
 
-            $("#goTop").click(function (e) {
-                e.preventDefault();
-                $("html,body").animate({ scrollTop: 0 }, 500);
+                $("#goTop").click(function (e) {
+                    e.preventDefault();
+                    $("html,body").animate({ scrollTop: 0 }, 500);
 
+                });
             });
-        });
+
+        }
+    } else {
+        if (window.location.href.indexOf('index.html') != -1) {
+            //JSON that stores all posts in JSON format
+            let jsonPost = [
+                { title: 'title test 1', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 1' },
+                { title: 'title test 2', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 2' },
+                { title: 'title test 3', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 3' },
+                { title: 'title test 4', date: "Published on " + moment().format("MMMM Do YYYY"), content: 'this is the post content 4' }
+
+            ];
+            //Then each jsonPost goes into an HTML structure for it show
+            jsonPost.forEach(element => {
+                let htmlPost = `
+            <article class="post">
+                <h2>${element.title}</h2>
+                <span class="date">${element.date}</span>
+                <p>
+                  ${element.content}
+                </p>
+                <a href="#" class="button-more">Read more</a>
+              </article>
+              <hr />
+            `;
+                //adding every post
+                $("#posts").append(htmlPost);
+
+                $("#goTop").click(function (e) {
+                    e.preventDefault();
+                    $("html,body").animate({ scrollTop: 0 }, 500);
+
+                });
+            });
+
+        }
+
     }
 
+    if (onDomain()) {
+        if (window.location.href == 'https://junn3r.github.io/about.html') {
+            //accordion
+            $(function () {
+                $("#accordion").accordion({ heightStyle: "content" });
+            });
+        }
+    } else {
+        if (window.location.href.indexOf('about.html') != -1) {
+            $(function () {
+                $("#accordion").accordion({ heightStyle: "content" });
+            });
+        }
 
-    //Then each jsonPost goes into an HTML structure for it show
-
-
-
-    //accordion
-    if (window.location.href.indexOf('about.html') != -1 || window.location.href.indexOf('https://junn3r.github.io/about.html') != -1) {
-        $( function() {
-            $( "#accordion" ).accordion({heightStyle: "content"});
-          } );
     }
+
+    
+
 
 
 
